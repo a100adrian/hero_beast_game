30978c3e32dd6a257a01aad452cac9510f716950
a100adrian <33597851+a100adrian@users.noreply.github.com>
2021-05-05 21:27:12 +0300

    Update GameTest.php

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -8,45 +8,87 @@ use app\src\config\Config;
 use app\src\factory\ConcreteBeast;
 use PHPUnit\Framework\TestCase;
 use app\src\characters\AbstractCharacter;
-use app\src\core\api\GameInterface;
+use app\src\core\Game;
+use app\src\core\Logs;
+use app\src\factory\ConcreteSkill;
+use app\src\skills\Generator;
+use Mockery as m;
+use function PHPUnit\Framework\assertSame;
 
 class GameTest extends TestCase
 {
-    public function test_if_characters_are_instance_of_abstract_class()
+    private Game $subject;
+    private AbstractCharacter $model1;
+    private AbstractCharacter $model2;
+    private $m;
+    
+    public function setUp():void
     {
         $app = new Application();
+        $this->model1 = $app->buildCharacter(new ConcreteHero(new StatsGenerator(), Config::HERO_STATS));
+        $this->model1->setName('hero');
         
-        $model1 = $app->buildCharacter(new ConcreteHero(new StatsGenerator(), Config::HERO_STATS));
-        $model1->setName('name');
+        $this->model2 = $app->buildCharacter(new ConcreteBeast(new StatsGenerator(), Config::BEAST_STATS));
+        $this->model2->setName('beast');
         
-        $model2 = $app->buildCharacter(new ConcreteBeast(new StatsGenerator(), Config::BEAST_STATS));
-        $model2->setName('name');
+        $this->model1->buildSkills(new ConcreteSkill(new Generator(), Config::MAGIC_SHIELD));
+        $this->model1->buildSkills(new ConcreteSkill(new Generator(), Config::RAPID_STRIKE));
         
-        $subject = $app->game->setHero($model1);
-        $subject = $app->game->setBeast($model2);
+        $this->m = m::mock(Game::class, [new Config(), new Logs()])
+        ->makePartial();
         
-        $actual1 = $subject->getHero();
-        $actual2 = $subject->getBeast();
+        $app->game = $this->m;
+        $app->game->setHero($this->model1);
+        $app->game->setBeast($this->model2);
+        $app->game->begin();
         
-        $this->assertInstanceOf(AbstractCharacter::class, $actual1);
-        $this->assertInstanceOf(AbstractCharacter::class, $actual2);
+        $this->subject = $app->game;
+        
     }
     
-    public function test_if_game_is_an_instance_of_game_interface()
+    public function test_if_characters_are_instance_of_expected_class()
     {
-        $app = new Application();
         
-        $model1 = $app->buildCharacter(new ConcreteHero(new StatsGenerator(), Config::HERO_STATS));
-        $model1->setName('name');
+        $actual = $this->subject->getHero();
+        $this->assertInstanceOf(AbstractCharacter::class, $actual);
         
-        $model2 = $app->buildCharacter(new ConcreteBeast(new StatsGenerator(), Config::BEAST_STATS));
-        $model2->setName('name');
+        $actual = $this->subject->getBeast();
+        $this->assertInstanceOf(AbstractCharacter::class, $actual);
         
-        $subject = $app->game->setHero($model1);
+        $actual = $this->subject->getAttacker();
+        $this->assertInstanceOf(AbstractCharacter::class, $actual);
         
-        $actual = $subject->getHero();
+        $actual = $this->subject->getDefender();
+        $this->assertInstanceOf(AbstractCharacter::class, $actual);
         
-        $this->assertInstanceOf(GameInterface::class, $app->game);
+        $actual = $this->subject->getWinner();
+        $this->assertInstanceOf(AbstractCharacter::class, $actual);
+    }
+   
+    public function test_if_setFirstAttacker_sets_correct_roles()
+    {
+        $this->model1->setSpeed(2);
+        $this->model2->setSpeed(2);
+        
+        $this->model1->setLuck(3);
+        $this->model2->setLuck(1);
+        
+        $this->m->shouldReceive('setFirstAttacker')
+        ->andReturn(false);
+                
+        $this->subject->setFirstAttacker();
+        
+        $this->assertEquals($this->subject->getAttacker()->getName(), 
+            $this->subject->getHero()->getName());
+        
+        $this->assertEquals($this->subject->getAttacker()->getName(), 
+            $this->subject->getHero()->getName());
+    }
+    
+    function tearDown():void
+    {
+        parent::tearDown();
+        $container = m::getContainer();
+        $this->addToAssertionCount($container->mockery_getExpectationCount());
     }
 }
-
